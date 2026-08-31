7977a56aebbd59c0a06f6e038e46da60cefca699
arun377 <arunkarthi887@gmail.com>
2026-08-31 16:46:07 +0530

    added firecrawl mcp

--- app/tools/mcp_servers.py
+++ app/tools/mcp_servers.py
@@ -2,5 +2,9 @@ MCP_SERVERS = {
     "deepwiki": {
         "url": "https://mcp.deepwiki.com/mcp",
         "transport": "streamable_http",
-    }
+    },
+    "firecrawl": {
+            "url": "https://mcp.firecrawl.dev/v2/mcp",
+            "transport": "streamable_http",
+        }
 }
